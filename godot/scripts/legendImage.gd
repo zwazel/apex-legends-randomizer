fbd9b6b7b71aa5d6c5e2e516553d2c6891ffccb4
Tim Irmler - Gamer PC <zwazel@hotmail.com>
2020-11-15 15:11:24 +0100

    basic selecting

--- godot/scripts/legendImage.gd
+++ godot/scripts/legendImage.gd
@@ -1,6 +1,19 @@
-extends VBoxContainer
+extends CenterContainer;
 
-export (String) var legendName;
+export (String) var legendName = "placeholder";
+
+var mouseEntered = false;
 
 func _ready():
-	$Label.text = legendName;
+	$legendImage/Label.text = legendName;
+
+func _input(event):
+	if mouseEntered:
+		if (event.is_pressed() and event.button_index == BUTTON_LEFT):
+			print("Selected Legend = " + legendName);
+
+func _on_Node_mouse_entered():
+	mouseEntered = true;
+
+func _on_Node_mouse_exited():
+	mouseEntered = false;
